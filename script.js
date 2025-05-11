64dcfa2887d3c0ce73b03b07fb6695a697894c80
Lalu Fatan <lalufatan25@gmail.com>
2025-05-11 14:28:51 +0700

    tes

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // script.js
 document.addEventListener("DOMContentLoaded", function () {
     setTimeout(() => {
-      alert("🎌 Promo Kursus Jepang! Dapatkan diskon 30% untuk paket premium NihongoClass!");
+      alert("🎌 Promo Kursus Jepang! Dapatkan diskon 100% untuk paket premium NihongoClass!");
     }, 2000);
-  });
+
+    const audio = document.getElementById('bg-music');
+    const muteBtn = document.getElementById('mute-btn');
   
+    let isMuted = false;
+  
+    muteBtn.addEventListener('click', function () {
+      isMuted = !isMuted;
+      audio.muted = isMuted;
+      muteBtn.textContent = isMuted ? '🔇' : '🔈';
+    });
+});
