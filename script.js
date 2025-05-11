c8d270d4b39824aad25455147ceae885ac07767d
Lalu Fatan <lalufatan25@gmail.com>
2025-05-11 12:07:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(() => {
+      alert("🎌 Promo Kursus Jepang! Dapatkan diskon 30% untuk paket premium NihongoClass!");
+    }, 2000);
+  });
+  
